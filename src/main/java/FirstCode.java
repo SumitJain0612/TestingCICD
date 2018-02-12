eb0a07124026d95b54865b56eddbf78687f5e847
sjain <sjain@ooyala.com>
2018-02-12 15:18:26 -0600

    first commit

---
+++ src/main/java/FirstCode.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sjain on 2/12/18.
+ */
+public class FirstCode {
+
+    public static void main(String[] args) {
+        System.out.println("In Main class");
+    }
+}
---
+++ src/test/java/testing.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by sjain on 2/12/18.
+ */
+public class testing {
+
+    @Test
+    public void printName() {
+        System.out.println("Sumit Jain");
+    }
+}
