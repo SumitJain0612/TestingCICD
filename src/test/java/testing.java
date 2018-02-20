02872f103a05520f9c2431f2e865a778afc23810
sjain <sjain@ooyala.com>
2018-02-20 12:00:59 -0600

    new chnagedddd new

--- src/test/java/testing.java
+++ src/test/java/testing.java
@@ -12,6 +12,6 @@ public class testing {
 
     @Test
     public void message() {
-        System.out.println("Saying hi to testingggg new new");
+        System.out.println("Saying hi to testingggg anvitha");
     }
 }
